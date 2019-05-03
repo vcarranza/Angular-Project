063fd4181308a7cdf6e7e6e6467cd965d38ced84
vcarranza <viccarranza67@gmail.com>
2019-05-03 11:33:45 -0700

    updates

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Trust Recipe', 'This is simply a test','https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
